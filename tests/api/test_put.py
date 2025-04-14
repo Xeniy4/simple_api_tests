3cd30bc13caae6c4e8ff57195be19b180e3144cb
Goryaeva Kseniya <goryaevakss@gmail.com>
2025-04-14 19:01:50 +0300

    add put tests update

--- tests/api/test_put.py
+++ tests/api/test_put.py
@@ -7,21 +7,22 @@ from schemas import put_update_user
 base_url = "https://reqres.in"
 endpoint_update = "/api/users/2"
 
-body_update_job = {
+body_update = {
     "name": "Tom",
     "job": "zion resident"
 }
 
+
+
 def test_put_user():
-    response = requests.put(base_url + endpoint_update, data=body_update_job)
+    response = requests.put(base_url + endpoint_update, data=body_update)
     assert response.status_code == 200
     assert response.json()["job"] == "zion resident"
     assert response.json()["name"] == "Tom"
     response_body = response.json()
     validate(response_body, put_update_user)
 
-def test_put_user_negative():
-    pass
+
 # добавить изменение имени в боди, и еще что-нибудь
 # изменить имя или работу невалидными значениями
 
